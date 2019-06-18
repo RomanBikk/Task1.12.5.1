0a8dd598c2e8057c65f9e8d0a467bf3e922711e0
RomanBikk <dallas-113@tut.by>
2019-06-18 18:05:06 +0200

    Вывод первых 10-ти чисел последовательности типа : 2, 4, 6, 8

---
+++ src/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        int count = 0;
+        int i = 1;
+        do {
+            i*=2;
+            System.out.println(i);
+
+            count++;
+        }
+        while (count<10);
+    }
+}
